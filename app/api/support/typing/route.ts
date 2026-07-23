feb039220fa005e683d46ecca86eaf5dcaddffb8
JejekingYT <jeromeboehme15@gmail.com>
2026-07-23 17:56:26 +0200

    Fixed Support Typing...

--- app/api/support/typing/route.ts
+++ app/api/support/typing/route.ts
@@ -1,25 +1,50 @@
 import { NextResponse } from "next/server";
 import { pusher } from "@/lib/pusher";
 
+
 export async function POST(req: Request) {
 
   const body = await req.json();
 
-  const { ticketId, username, role } = body;
+
+  const {
+    ticketId,
+    userId,
+    username,
+    role,
+  } = body;
+
 
 
   await pusher.trigger(
+
     `ticket-${ticketId}`,
+
     "typing",
+
     {
-      username,
-      role,
+
+      userId,
+
+      name:
+        role === "SUPPORT" ||
+        role === "ADMIN" ||
+        role === "OWNER"
+
+          ? "🎫 Nexus Support"
+
+          : username,
+
     }
+
   );
 
 
+
   return NextResponse.json({
+
     success: true,
+
   });
 
 }
