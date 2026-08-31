e2cc10e2b8bcd3dadb6e58d5b4949fb0b6dec822
Dennis-Marcel Godglück <dennisgodglueck@gmail.com>
2026-08-31 14:09:06 +0200

    fixed component error

--- lib/settings.ts
+++ lib/settings.ts
@@ -3,21 +3,21 @@ import "server-only";
 import { prisma } from "@/lib/prisma";
 
 export async function getSiteSettings() {
-  let settings = await prisma.siteSettings.findUnique({
-    where: {
-      id: 1,
-    },
-  });
+let settings = await prisma.siteSettings.findUnique({
+where: {
+id: 1,
+},
+});
 
-  if (!settings) {
-    settings = await prisma.siteSettings.create({
-      data: {
-        id: 1,
-        siteName: "Nexus",
-        description: "Nexus Community",
-      },
-    });
-  }
+if (!settings) {
+settings = await prisma.siteSettings.create({
+data: {
+id: 1,
+siteName: "Nexus",
+description: "Nexus Community",
+},
+});
+}
 
-  return settings;
+return settings;
 }
